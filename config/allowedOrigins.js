dcfe16f921c2b996e6c175ae005be28ae05301c9
an196 <an9x0010@gmail.com>
2023-01-30 21:38:01 +0700

    add new allow origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
     'http://localhost:8080',
     'http://localhost:3000',
     'http://localhost:3001',
+    'https://63d7d5aa33a46525e508115b--melodious-bublanina-4edb52.netlify.app/'
 ];
 
 module.exports = allowedOrigins;
