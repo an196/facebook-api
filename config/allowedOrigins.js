e5439aaba252722eeb76f6b62fe10ae561aecbe9
an196 <an9x0010@gmail.com>
2023-01-30 21:48:43 +0700

    fix name allow origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,7 @@ const allowedOrigins = [
     'http://localhost:8080',
     'http://localhost:3000',
     'http://localhost:3001',
-    'https://63d7d5aa33a46525e508115b--melodious-bublanina-4edb52.netlify.app'
+    'https://melodious-bublanina-4edb52.netlify.app'
 ];
 
 module.exports = allowedOrigins;
